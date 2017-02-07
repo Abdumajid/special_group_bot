5f4bf017a36db3c578096788dbdfdd8719814bb0
mirjalolNorqulov <mirjalol2401@gmail.com>
2017-02-07 11:27:06 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,9 +15,8 @@ def start(message):
 
 @bot.message_handler(commands = ['help'])
 def help(message):
-	text = "Welcome!\nPlease use below the menu \xE2\xAC\x87"
-	bot.send_message(message.chat.id, text = text)
-
+	caption = "Can I help you?\nPlease choose one of the weekdays from the menu below \xE2\xAC\x87"
+	bot.send_photo(message.chat.id, "https://thumb9.shutterstock.com/display_pic_with_logo/3075959/269077082/stock-photo-two-hands-helping-hand-to-a-friend-269077082.jpg", caption = caption)
 
 @bot.message_handler(func = lambda message: message.text in markups.days)
 def show_table(message):
